0218e927cf6e99473d25ae4f116bb6e46c93e8f4
Rob Moore <moore.re@gmail.com>
2019-01-03 23:09:56 -0600

    Started own rock paper scissors project.

---
+++ FCC_js/rock_paper_scissors.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){
+  console.log('We are live!');
+
+  $('#rock').click(function(){
+    console.log('You chose rock!');
+  })
+
+  $('#paper').click(function(){
+    console.log('You chose paper!');
+  })
+
+  $('#scissors').click(function(){
+    console.log('You chose scissors!');
+  })
+
+});
