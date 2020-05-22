490e410fc1ce642f083cd5f10c812a6b4e5d56a8
Rob Moore <moore.re@gmail.com>
2020-05-21 19:30:40 -0500

    Added Booklist exercise

--- FCC_js/JSprojects.js
+++ FCC_js/JSprojects.js
@@ -10,10 +10,39 @@ function palindrome(str) {
 palindrome("A man, a plan, a canal. Panama");
 // returns true
 
+
+// Start of Booklist project
+// The global variable
+var bookList = ["The Hound of the Baskervilles", "On The Electrodynamics of Moving Bodies", "Philosophiæ Naturalis Principia Mathematica", "Disquisitiones Arithmeticae"];
+
+  // Start of Change
+function add (arr, bookName) {
+  let newBookList = [...arr];
+  newBookList.push(bookName);
+  return newBookList;
+  }
+
+function remove (arr, bookName) {
+  let newBookList = [...arr];
+  if (newBookList.indexOf(bookName) >= 0) {
+    newBookList.splice(newBookList.indexOf(bookName), 1); 
+    return newBookList;
+
+    }
+}
+  // End of Change
+
+var newBookList = add(bookList, 'A Brief History of Time');
+var newerBookList = remove(bookList, 'On The Electrodynamics of Moving Bodies');
+var newestBookList = remove(add(bookList, 'A Brief History of Time'), 'On The Electrodynamics of Moving Bodies');
+
+console.log(bookList);
+// End of Booklist project
+
 // FCC Roman Numeral Converter
 function romanNumeralConverter(num){
   if (num !== isNan()) {
-    
+
   } else {
     return "Not a number";
   }
